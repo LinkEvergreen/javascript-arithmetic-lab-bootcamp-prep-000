d5eda60e069e336c244bd44e3857d8953cadaa8b
David MacPhail <david.macphail@icloud.com>
2020-05-20 18:06:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ function increment(a) {
 
 function makeInt(n) {
    return parseInt('0x2328', 10)
-}
-
-function makeInt(n) {
-  return parseInt("n", 10)
+   else parseInt('a', 10)
+   else parseInt("a")
+   }
 }
